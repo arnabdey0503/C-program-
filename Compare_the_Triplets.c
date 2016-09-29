e46e4ade389430d83ba6c8d0ccd80cc6400efe50
Arnab Dey <arnabdeyuttarpara@gmail.com>
2016-09-29 22:14:44 +0530

    first

---
+++ Compare_the_Triplets.c
@@ -0,0 +1,19 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main(){
+    int a0; 
+    int a1; 
+    int a2; 
+    scanf("%d %d %d",&a0,&a1,&a2);
+    int b0; 
+    int b1; 
+    int b2; 
+    scanf("%d %d %d",&b0,&b1,&b2);
+    return 0;
+}
